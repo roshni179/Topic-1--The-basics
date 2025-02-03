263ee86daf5c3932ffe036bcef0240b11896a87a
701030552 <701030552@2260I-L29-031>
2025-02-03 14:32:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_1__The_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
