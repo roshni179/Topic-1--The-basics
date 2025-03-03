66c4db4d24f2b740f41c8cae562788b3e0268f84
701030552 <701030552@2260I-L29-031>
2025-03-03 13:30:44 -0500

    done topic 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,34 @@
-﻿namespace Topic_1__The_basics
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // Roshni
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string firstName = "Roshni";
+string favMovie = "The Matrix";
+
+string quote = "She's our friend and she's crazy";
+
+Console.WriteLine("Hello  " + firstName + " "+ favMovie.ToUpper());
+favMovie = (favMovie.ToUpper());
+Console.WriteLine(favMovie.Contains("THE"));
+
+favMovie = favMovie.Replace("A", "@");
+favMovie = favMovie.Replace("E", "3");
+Console.WriteLine(favMovie);
+Console.WriteLine();
+
+Console.WriteLine(quote);
+quote = quote.ToUpper();
+Console.WriteLine(quote);
+
+quote = quote.Replace("A"," ");
+quote = quote.Replace("E"," ");
+quote = quote.Replace("I"," ");
+quote = quote.Replace("O"," ");
+quote = quote.Replace("U"," ");
+Console.WriteLine(quote);
+
+
+Console.WriteLine();
+Console.WriteLine("           /\\                 __                ____                  ");
+Console.WriteLine("       .--/ --\\--,           /\\ \\              /\\___\\             ");
+Console.WriteLine("        \\/    \\/            / /\\ \\            /\\ \\___\\          ");
+Console.WriteLine("        /\\    /\\           / /__\\ \\           \\ \\/ / /           ");
+Console.WriteLine("       '--\\--/--`          \\/____\\/            \\/_/_/              ");
+Console.WriteLine("           \\/                                                         ");
+
